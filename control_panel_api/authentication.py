d3d7b379f70cb460322757e2f9f0ad22f1607788
James Morris <james@jmoz.co.uk>
2017-08-25 13:59:31 +0100

    Proof of concept auth0 jwt backend with tests. Given a valid token with username sub will allow authentication and return user

--- control_panel_api/authentication.py
+++ control_panel_api/authentication.py
@@ -1,47 +1,35 @@
-from django.conf import settings
 import jwt
+from django.conf import settings
 from jwt.exceptions import InvalidTokenError
-from rest_framework.authentication import (
-    BaseAuthentication,
-    get_authorization_header
-)
+from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
 from control_panel_api.models import User
 
 
 class Auth0JWTAuthentication(BaseAuthentication):
-
     def authenticate(self, request):
-
         try:
-            prefix, token = get_authorization_header(request).split()
-
+            prefix, token = request.META.get('HTTP_AUTHORIZATION', '').split()
         except (ValueError, TypeError):
             return None
 
-        if prefix != 'JWT'.encode():
+        if prefix != 'JWT':
             raise AuthenticationFailed('JWT prefix missing')
 
         try:
-            decoded = jwt.decode(
-                token,
-                key=settings.OIDC_CLIENT_SECRET,
-                audience=settings.OIDC_CLIENT_ID
-            )
-
-        except InvalidTokenError as error:
-            raise AuthenticationFailed(error)
-
-        sub = decoded.get('sub')
-
-        if sub is None:
-            raise AuthenticationFailed('JWT missing "sub" field')
+            decoded = jwt.decode(token, key=settings.AUTH0_SECRET, audience=settings.AUTH0_AUDIENCE)
+        except InvalidTokenError:
+            raise AuthenticationFailed('JWT decode error')
 
         try:
-            user = User.objects.get(auth0_id=sub)
+            username = decoded['sub']
+        except KeyError:
+            raise AuthenticationFailed('JWT missing sub username field')
 
+        try:
+            user = User.objects.get(username=username)
         except User.DoesNotExist:
-            raise AuthenticationFailed(f'No such user: {sub}')
+            raise AuthenticationFailed('No such user')
 
-        return user, None
+        return (user, None)
