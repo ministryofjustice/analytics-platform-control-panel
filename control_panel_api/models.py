47e2c9131ed95003d3d315c94e95e4d16197099f
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-08-25 12:26:19 +0100

    Added Team, Role and TeamMembership models
    The `TeamMembership` model keep the relationship between teams, users and
    roles.
    A user belongs to a team with a given role (e.g. "maintainer" or "member").
    
    A user can be in a team only once and only with a single role:
      * add a user to a team more than once doesn't make any sense
      * add a user to a team with more than one role is confusing and doesn't
        make any sense (e.g. has the user the "member" role or the "maintainer"
        role?)
    
    I'll add the following methods to Team and User to make the API a bit more
    user friendly:
      * `User#teams` to return the teams a user is in
      * `Team#users` to return the users in a team
      * `Team#users_with_role` to return the users in a team with a certain
        role

--- control_panel_api/models.py
+++ control_panel_api/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django_extensions.db.fields import AutoSlugField, CreationDateTimeField
+from django_extensions.db.fields import AutoSlugField, CreationDateTimeField, ModificationDateTimeField
 
 
 class User(AbstractUser):
@@ -24,3 +24,41 @@ class App(models.Model):
 
     class Meta:
         ordering = ('name',)
+
+
+class Role(models.Model):
+    created = CreationDateTimeField()
+    code = models.CharField(max_length=256, blank=False, unique=True)
+
+    class Meta:
+        ordering = ('code',)
+
+
+class Team(models.Model):
+    created = CreationDateTimeField()
+    name = models.CharField(max_length=256, blank=False)
+    slug = AutoSlugField(populate_from='name')
+
+    class Meta:
+        ordering = ('name',)
+
+
+class TeamMembership(models.Model):
+    """
+    User's membership to a team. A user is member of a team with a
+    given role, e.g. user_1 is maintainer (role) in team_1
+    """
+
+    created = CreationDateTimeField()
+    modified = ModificationDateTimeField()
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            # a user can be in a team only once and with exactly one role
+            ('user', 'team', 'role'),
+            ('user', 'team'),
+        )
