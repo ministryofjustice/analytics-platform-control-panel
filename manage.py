51f9eeed18b57c38f16142a8aaa278540ff6f739
James Morris <james@jmoz.co.uk>
2017-09-18 16:39:50 +0100

    Default manage test to use test settings

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control_panel_api.settings")
+    settings = 'control_panel_api.settings.test' if 'test' in sys.argv else 'control_panel_api.settings'
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
