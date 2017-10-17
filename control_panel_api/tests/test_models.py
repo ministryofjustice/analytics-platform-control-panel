cabbd7c78f74b72f6ddce29935864fd058f9b000
James Morris <james@jmoz.co.uk>
2017-10-17 11:29:37 +0100

    Revert "Use auth0_id in aws role"
    Will break existing
    
    This reverts commit ebf5444d82c35c99fa1cc7fd2fdc5cd642e9b12c.

--- control_panel_api/tests/test_models.py
+++ control_panel_api/tests/test_models.py
@@ -18,11 +18,11 @@ from control_panel_api.tests import APP_IAM_ROLE_ASSUME_POLICY
 class UserTestCase(TestCase):
     @patch('control_panel_api.services.create_role')
     def test_aws_create_role_calls_service(self, mock_create_role):
-        auth0_id = 'github|1'
-        user = User.objects.create(auth0_id=auth0_id)
+        username = 'james'
+        user = User.objects.create(username=username)
         user.aws_create_role()
 
-        expected_role_name = f"test_user_{auth0_id}"
+        expected_role_name = f"test_user_{username}"
 
         mock_create_role.assert_called_with(
             expected_role_name,
@@ -31,11 +31,11 @@ class UserTestCase(TestCase):
 
     @patch('control_panel_api.services.delete_role')
     def test_aws_delete_role_calls_service(self, mock_delete_role):
-        auth0_id = 'github|1'
-        user = User.objects.create(auth0_id=auth0_id)
+        username = 'james'
+        user = User.objects.create(username=username)
         user.aws_delete_role()
 
-        expected_role_name = f"test_user_{auth0_id}"
+        expected_role_name = f"test_user_{username}"
 
         mock_delete_role.assert_called_with(expected_role_name)
 
