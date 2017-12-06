243816b02aa6722346ef2f9f7c53cb68d9c4692b
James Morris <james@jmoz.co.uk>
2017-12-06 14:52:35 +0000

    Self document variable

--- control_panel_api/tests/test_pagination.py
+++ control_panel_api/tests/test_pagination.py
@@ -7,23 +7,31 @@ from control_panel_api.tests.test_views import AuthenticatedClientMixin
 
 class TestPagination(AuthenticatedClientMixin, APITestCase):
     def test_pagination(self):
-        mommy.make('control_panel_api.App', 120)  # 120 exceeds the 100 default
+        default_page_size = 100
+
+        mommy.make('control_panel_api.App', default_page_size + 20)
 
         app_list_url = reverse('app-list')
 
         fixtures = (
-            (app_list_url, 100, True),  # default paging no query param
-            (app_list_url + '?page_size=50', 50, True),  # less than default
-            (app_list_url + '?page_size=130', 120, False),  # more than default
-            (app_list_url + '?page_size=0', 120, False),  # return all
+            (app_list_url, default_page_size, True, False),
+            (f'{app_list_url}?page_size={default_page_size - 50}', 50, True, False),
+            (f'{app_list_url}?page_size={default_page_size - 50}&page=3', 20, False, True),
+            (f'{app_list_url}?page_size={default_page_size + 30}', 120, False, False),
+            (f'{app_list_url}?page_size=0', 120, False, False),
         )
 
-        for url, expected_page_size, has_next in fixtures:
+        for url, expected_page_size, has_next, has_prev in fixtures:
             response = self.client.get(url)
             self.assertEqual(120, response.data['count'])
             self.assertEqual(expected_page_size, len(response.data['results']))
-            self.assertIsNone(response.data['previous'])
+
             if has_next:
                 self.assertIsNotNone(response.data['next'])
             else:
                 self.assertIsNone(response.data['next'])
+
+            if has_prev:
+                self.assertIsNotNone(response.data['previous'])
+            else:
+                self.assertIsNone(response.data['previous'])
