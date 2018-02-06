2e2bf10c1feec800a98601db7fa5679a2e4ae11e
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-02-06 11:21:44 +0000

    OIDC migration PR amendments
    - Inverted logic to check for True not False
    - OIDC_PROVIDER_ARN is a constant now, not a method
    - Moved `_has_oidc_statement()` and `_oidc_statement()` functions outside the
      class

--- control_panel_api/management/commands/add_oidc_statement_to_users_roles.py
+++ control_panel_api/management/commands/add_oidc_statement_to_users_roles.py
@@ -8,6 +8,9 @@ from django.core.management.base import BaseCommand
 from control_panel_api.models import User
 
 
+OIDC_PROVIDER_ARN = f'{settings.IAM_ARN_BASE}:oidc-provider/{settings.OIDC_DOMAIN}/'
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,9 +24,11 @@ class Command(BaseCommand):
             iam_role = iam.Role(user.iam_role_name)
             assume_role_policy_document = iam_role.assume_role_policy_document
 
-            if not self._has_oidc_statement(assume_role_policy_document):
+            if _has_oidc_statement(assume_role_policy_document):
+                logger.info(f'OIDC statement already found in "{user.iam_role_name}" trust policy. Skipping.')
+            else:
                 # Add OIDC statement
-                oidc_statement = self._oidc_statement(user.auth0_id)
+                oidc_statement = _oidc_statement(user.auth0_id)
                 assume_role_policy_document['Statement'].append(oidc_statement)
 
                 # Update role trust policy
@@ -32,34 +37,30 @@ class Command(BaseCommand):
                     PolicyDocument=json.dumps(assume_role_policy_document)
                 )
                 logger.info(f'OIDC statement added to "{user.iam_role_name}" trust policy.')
-            else:
-                logger.info(f'OIDC statement already found in "{user.iam_role_name}" trust policy. Skipping.')
 
-    def _oidc_statement(self, oidc_sub):
-        return {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": self._oidc_provider_arn,
+
+def _has_oidc_statement(document):
+    for statement in document['Statement']:
+        try:
+            oidc_provider_arn = statement['Principal']['Federated']
+            if oidc_provider_arn == OIDC_PROVIDER_ARN:
+                return True
+        except KeyError:
+            pass
+
+    return False
+
+
+def _oidc_statement(oidc_sub):
+    return {
+        "Effect": "Allow",
+        "Principal": {
+            "Federated": OIDC_PROVIDER_ARN,
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+            "StringEquals": {
+                f"{settings.OIDC_DOMAIN}/:sub": oidc_sub,
             },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringEquals": {
-                    f"{settings.OIDC_DOMAIN}/:sub": oidc_sub,
-                },
-            },
-        }
-
-    def _has_oidc_statement(self, document):
-        for statement in document['Statement']:
-            try:
-                oidc_provider_arn = statement['Principal']['Federated']
-                if oidc_provider_arn == self._oidc_provider_arn:
-                    return True
-            except KeyError:
-                pass
-
-        return False
-
-    @property
-    def _oidc_provider_arn(self):
-        return f"{settings.IAM_ARN_BASE}:oidc-provider/{settings.OIDC_DOMAIN}/"
+        },
+    }
