ac2321f51be324ad1572bd55488c97f4808589f8
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-03-26 16:22:15 +0100

    Lambda migrations: Add task to dump policies attached to users' IAM roles
    This can be run before the actual data migration to save the state of
    IAM roles and attached policies. This can be useful to investigate possible
    problems after the migration, e.g. if a user says he can't access data
    anymore, etc...

---
+++ control_panel_api/management/commands/migrate_lambda_data_0_dump_user_policies.py
@@ -0,0 +1,58 @@
+import json
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+from django.core.management.base import BaseCommand
+
+from control_panel_api.models import User
+
+
+READWRITE = 'readwrite'
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    NOTE: Needs permission to perform `iam:ListAttachedRolePolicies` action
+    """
+
+
+    help = ("For each of the users in the DB, it collects its attached IAM "
+            "policies and prints this data to stdout")
+
+    def handle(self, *args, **options):
+        iam = boto3.client('iam')
+
+        dump = {}
+
+        for user in User.objects.all():
+            username = user.username
+            role_name = user.iam_role_name
+
+            dump[username] = {
+                'iam_role_name': role_name,
+                'attached_policies': [],
+            }
+
+            try:
+                policies = iam.list_attached_role_policies(
+                    RoleName=role_name,
+                    MaxItems=1000,
+                )
+                if policies:
+                    dump[username]['attached_policies'] = policies["AttachedPolicies"]
+            except ClientError as e:
+                if e.response['Error']['Code'] in ('NoSuchEntity', 'ValidationError'):
+                    logger.warning(
+                        f'Error reading policies from IAM role "{role_name}": '
+                        f'user "{user.username}" ("{user.pk}"): {e}'
+                    )
+                    dump[username]['error'] = str(e)
+                    continue
+                else:
+                    raise e
+
+        print(json.dumps(dump, indent=4))
