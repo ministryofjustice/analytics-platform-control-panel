f8456a486ca42c4f18d91d3a88cff8967efb4d2f
James Morris <james@jmoz.co.uk>
2018-05-02 12:12:53 +0100

    Import as _Elasticsearch

--- control_panel_api/elasticsearch.py
+++ control_panel_api/elasticsearch.py
@@ -1,11 +1,11 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch as _Elasticsearch
 from elasticsearch_dsl import Q, Search
 from elasticsearch_dsl.query import Range
 
 
 class ElasticSearch(object):
     def __init__(self, connection):
-        self.client = Elasticsearch(connection)
+        self.client = _Elasticsearch(connection)
 
     def bucket_hits_aggregation(self, index, bucket_name, day_range=None):
         s = Search(using=self.client, index=index)
