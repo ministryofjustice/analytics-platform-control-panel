24c692411685f5006807b3676fb26232612019af
Andy Driver <andy@pagezero.net>
2018-05-17 15:23:23 +0100

    Tidy up test output

--- control_panel_api/settings/test.py
+++ control_panel_api/settings/test.py
@@ -8,3 +8,5 @@ IAM_ARN_BASE = 'arn:aws:iam::123'
 K8S_WORKER_ROLE_NAME = 'test-k8s-worker-role'
 SAML_PROVIDER = 'test'
 OIDC_DOMAIN = 'auth.example.com'
+
+LOGGING['handlers']['console']['level'] = 'CRITICAL'
