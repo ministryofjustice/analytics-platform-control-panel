c727803d9a03cfd047d83d55d38c8a19bffc02ec
Andy Driver <andy@pagezero.net>
2018-05-30 14:29:35 +0100

    Pass elasticsearch settings correctly

--- control_panel_api/settings/test.py
+++ control_panel_api/settings/test.py
@@ -9,4 +9,15 @@ K8S_WORKER_ROLE_NAME = 'test-k8s-worker-role'
 SAML_PROVIDER = 'test'
 OIDC_DOMAIN = 'auth.example.com'
 
+ELASTICSEARCH = {
+    'hosts': [
+        {
+            'host': 'example.com',
+            'port': '9200',
+            'http_auth': ('test_user', 'password'),
+        },
+    ],
+    'index': 's3logs-*'
+}
+
 LOGGING['handlers']['console']['level'] = 'CRITICAL'
