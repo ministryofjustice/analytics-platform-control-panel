2f3848285ead4cae29028e386e7c9281058dabe3
Ravi Kotecha <kotecha.ravi@gmail.com>
2019-10-03 19:58:10 +0100

    Bugfix: test files in app.js bundle
    This commit excludes the test files from being bundled by babel. This bug was
    introduced by https://github.com/ministryofjustice/analytics-platform-control-panel/pull/755

--- jest.config.js
+++ jest.config.js
@@ -176,9 +176,10 @@ module.exports = {
   // transform: null,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "\\.test\\.js"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
