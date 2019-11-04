cfe0bcf923cedc1f59e04c1036f0ddd4cb1ae86f
Andy Driver <andy@pagezero.net>
2019-11-04 12:06:44 +0000

    Silently fail if no Slack API token is available

--- controlpanel/api/slack.py
+++ controlpanel/api/slack.py
@@ -3,10 +3,12 @@ from slack import WebClient
 
 
 def notify_team(msg):
-    WebClient(token=settings.SLACK["api_token"]).chat_postMessage(
-        as_user=False,
-        username=f"Control Panel [{settings.ENV}]",
-        channel=settings.SLACK["channel"],
-        text=msg,
-    )
+    token = settings.SLACK.get('api_token')
+    if token:
+        WebClient(token=token).chat_postMessage(
+            as_user=False,
+            username=f"Control Panel [{settings.ENV}]",
+            channel=settings.SLACK["channel"],
+            text=msg,
+        )
 
