1dac0fb5498d230fe0e45dd0f8bbc29e31e19719
Andy Driver <andyhd@users.noreply.github.com>
2019-11-12 10:47:53 +0000

    Notify team slack on creation of superuser (#770)
    * Notify team slack on creation of superuser
    
    * Silently fail if no Slack API token is available
    
    * Fix tests
    
    * Require API token and mock API calls in tests
    
    * Only send notifications from User.save() method
    
    * Raise exception if Slack token not set
    
    Crash out on startup if SLACK_API_TOKEN environment variable is not set.
    
    * Fix collectstatic
    
    Set a dummy SLACK_API_TOKEN env var to allow collectstatic to run when building the
    docker image and the environment variable has not been set yet
    
    * Code review amends
    
    * Remove unused parameter

---
+++ tests/api/test_slack.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch
+
+import pytest
+
+from controlpanel.api import slack
+
+
+@pytest.fixture
+def message_sent(settings, slack_WebClient):
+    def check_sent(message):
+        slack_WebClient.assert_called_with(token=settings.SLACK["api_token"])
+        slack_WebClient.return_value.chat_postMessage.assert_called_with(
+            as_user=False,
+            username=f"Control Panel [{settings.ENV}]",
+            channel=settings.SLACK["channel"],
+            text=message,
+        )
+        return True
+    return check_sent
+
+
+def test_send_notification(message_sent):
+    message = "test message"
+    slack.send_notification(message)
+
+    assert message_sent(message)
+
+@pytest.mark.parametrize(
+    "request_user",
+    [
+        None,
+        "normal_user",
+    ],
+    ids=[
+        "no-request-user",
+        "with-request-user",
+    ]
+)
+def test_notify_superuser_created(message_sent, users, request_user):
+    user = users['other_user']
+    expected_message = slack.CREATE_SUPERUSER_MESSAGE.format(username=user.username)
+    by_username = None
+    if request_user:
+        by_username = users[request_user].username
+        expected_message = f"{expected_message} by `{by_username}`"
+    slack.notify_superuser_created(user.username, by_username=by_username)
+
+    assert message_sent(expected_message)
