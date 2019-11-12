099db8e752f8ccc4a3cf8c957499965afc14a452
Andy Driver <andy@pagezero.net>
2019-11-12 10:21:29 +0000

    Remove unused parameter

--- tests/api/test_slack.py
+++ tests/api/test_slack.py
@@ -5,35 +5,38 @@ import pytest
 from controlpanel.api import slack
 
 
-def test_slack_send_notification(settings, slack_WebClient):
-    slack.send_notification("test message")
-    slack_WebClient.assert_called_with(token=settings.SLACK["api_token"])
-    slack_WebClient.return_value.chat_postMessage.assert_called_with(
-        as_user=False,
-        username=f"Control Panel [{settings.ENV}]",
-        channel=settings.SLACK["channel"],
-        text="test message",
-    )
+@pytest.fixture
+def message_sent(settings, slack_WebClient):
+    def check_sent(message):
+        slack_WebClient.assert_called_with(token=settings.SLACK["api_token"])
+        slack_WebClient.return_value.chat_postMessage.assert_called_with(
+            as_user=False,
+            username=f"Control Panel [{settings.ENV}]",
+            channel=settings.SLACK["channel"],
+            text=message,
+        )
+        return True
+    return check_sent
 
+
+def test_send_notification(message_sent):
+    message = "test message"
+    slack.send_notification(message)
+
+    assert message_sent(message)
 
 @pytest.mark.parametrize(
-    "message, request_user",
+    "request_user",
     [
-        ("test", None),
-        ("test", "normal_user"),
+        None,
+        "normal_user",
     ],
     ids=[
         "no-request-user",
         "with-request-user",
     ]
 )
-def test_slack_notify_superuser_created(
-    settings,
-    slack_WebClient,
-    users,
-    message,
-    request_user,
-):
+def test_notify_superuser_created(message_sent, users, request_user):
     user = users['other_user']
     expected_message = slack.CREATE_SUPERUSER_MESSAGE.format(username=user.username)
     by_username = None
@@ -42,10 +45,4 @@ def test_slack_notify_superuser_created(
         expected_message = f"{expected_message} by `{by_username}`"
     slack.notify_superuser_created(user.username, by_username=by_username)
 
-    slack_WebClient.assert_called_with(token=settings.SLACK["api_token"])
-    slack_WebClient.return_value.chat_postMessage.assert_called_with(
-        as_user=False,
-        username=f"Control Panel [{settings.ENV}]",
-        channel=settings.SLACK["channel"],
-        text=expected_message,
-    )
+    assert message_sent(expected_message)
