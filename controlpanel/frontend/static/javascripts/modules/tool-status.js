c1706cbd3262862651c92f3e3e0635a6b19bd433
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2020-07-09 15:01:59 +0100

    More UX Improvements to Tools list page
    Disable "Deploy" button when it doesn't make sense to deploy, e.g.
    if no version selected or the selected version is the one already
    installed.

--- controlpanel/frontend/static/javascripts/modules/tool-status.js
+++ controlpanel/frontend/static/javascripts/modules/tool-status.js
@@ -15,6 +15,12 @@ moj.Modules.toolStatus = {
     if (toolStatusListeners) {
       this.bindEvents(toolStatusListeners);
     }
+
+    // Bind version selects' change event listeners
+    const versionSelects = document.querySelectorAll(this.versionSelector);
+    versionSelects.forEach(versionSelect => {
+      versionSelect.addEventListener("change", (event) => this.versionSelectChanged(event.target));
+    });
   },
 
   bindEvents(listeners) {
@@ -55,16 +61,18 @@ moj.Modules.toolStatus = {
 
   // Select the new version from the tool "version" select input
   updateAppVersion(listener, newVersion) {
+    const selectElement = listener.querySelector(this.versionSelector);
+
     if (newVersion) {
       // 1. remove "(not installed)" option
-      let notInstalledOption = listener.querySelector(this.versionSelector + " ." + this.versionNotInstalledClass);
+      let notInstalledOption = selectElement.querySelector("option." + this.versionNotInstalledClass);
 
       if (notInstalledOption) {
         notInstalledOption.remove();
       }
 
       // 2. remove "(installed)" suffix and class from old version
-      let oldVersionOption = listener.querySelector(this.versionSelector + " ." + this.versionInstalledClass);
+      let oldVersionOption = selectElement.querySelector("option." + this.versionInstalledClass);
 
       if (oldVersionOption) {
         oldVersionOption.innerText = oldVersionOption.innerText.replace(this.installedSuffix, "");
@@ -79,11 +87,29 @@ moj.Modules.toolStatus = {
         newVersionOption.classList.add(this.versionInstalledClass)
       }
     }
+
+    // After deploy, update select/deploy button
+    this.versionSelectChanged(selectElement);
   },
 
   showActions(listener, action_names) {
     listener.querySelectorAll(this.actionClass).forEach(action => {
       action.classList.toggle(this.hidden, !action_names.includes(action.dataset.actionName));
     });
-  }
+  },
+
+  // version select "change" event handler
+  versionSelectChanged(target) {
+    const selected = target.options[target.options.selectedIndex];
+    const classes = selected.className.split(" ");
+
+    const notInstalledSelected = classes.indexOf(this.versionNotInstalledClass) !== -1;
+    const installedSelected = classes.indexOf(this.versionInstalledClass) !== -1;
+
+    const deployButton = target.closest(`form${this.actionClass}[data-action-name='deploy']`).querySelector("button");
+
+    // If "(not installed)" or "(installed)" version selected
+    // the "Deploy" button needs to be disabled
+    deployButton.disabled = notInstalledSelected || installedSelected;
+  },
 };
