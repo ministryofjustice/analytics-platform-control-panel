dc00c495930981a791888953f3d69ceb9cf699d0
Nicholas H.Tollervey <ntoll@ntoll.org>
2020-09-08 12:50:48 +0100

    Svelte way to highlight and remove unused user accounts.

--- controlpanel/frontend/views/user.py
+++ controlpanel/frontend/views/user.py
@@ -1,4 +1,5 @@
 import dateutil.parser
+from datetime import datetime, timedelta
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -10,6 +11,15 @@ from rules.contrib.views import PermissionRequiredMixin
 
 from controlpanel.api import auth0
 from controlpanel.api.models import User
+
+
+def three_months_ago():
+    """
+    Returns a datetime object referencing approximately three months in the
+    past, from the current date. The assumption made here is that a month is
+    roughly 30 days (so the result is always 90 days in the past).
+    """
+    return datetime.now() - timedelta(days=30*3)
 
 
 class UserList(LoginRequiredMixin, PermissionRequiredMixin, ListView):
@@ -28,6 +38,11 @@ class UserList(LoginRequiredMixin, PermissionRequiredMixin, ListView):
             },
         )
 
+        auth0_mapper = {}
+        for db_user in self.queryset:
+            if db_user.auth0_id:
+                auth0_mapper[db_user.auth0_id] = db_user
+
         last_logins = []
         for user in users:
             auth0_id = user['user_id']
@@ -36,11 +51,20 @@ class UserList(LoginRequiredMixin, PermissionRequiredMixin, ListView):
                 continue
 
             try:
-                last_login = dateutil.parser.parse(user.get('last_login'))
+                auth0_last_login = user.get("last_login")
+                if auth0_last_login:
+                    last_login = dateutil.parser.parse(user.get('last_login'))
+                else: 
+                    db_user = auth0_mapper.get(auth0_id, None)
+                    if db_user:
+                        last_login = db_user.last_login
             except (TypeError, ValueError):
                 last_login = None
 
-            last_logins.append((auth0_id, last_login))
+            last_logins.append((auth0_id, {
+                "last_login": last_login,
+                "unused": last_login.utcnow() < three_months_ago(),
+            }))
 
         context['last_login'] = dict(last_logins)
 
@@ -61,6 +85,11 @@ class UserDetail(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = User
     permission_required = 'api.retrieve_user'
     template_name = "user-detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["unused"] = self.object.last_login.utcnow() < three_months_ago()
+        return context
 
 
 class SetSuperadmin(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
