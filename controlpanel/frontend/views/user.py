5a30c9ec6e85d7432699af5c38cc88f1dd6a33ff
Nicholas H.Tollervey <ntoll@ntoll.org>
2020-09-18 11:47:10 +0100

    User class based get_ordering from querystring.

--- controlpanel/frontend/views/user.py
+++ controlpanel/frontend/views/user.py
@@ -29,14 +29,17 @@ class UserList(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     queryset = User.objects.exclude(auth0_id='')
     template_name = "user-list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_ordering(self):
         order_by = self.request.GET.get("o", "username")
         valid_fields = ["username", "email", "last_login", ]
         if order_by not in valid_fields:
             order_by = "username"
+        return order_by
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         unused_users = []
-        for user in self.queryset.order_by(order_by):
+        for user in self.queryset:
             if user.last_login:
                 if user.last_login.date() < ninety_days_ago():
                     unused_users.append(user)
