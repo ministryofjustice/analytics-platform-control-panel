c35f81bdcb35ac1e8c7cc1c24469dd2dc32d0038
Gemma Hentsch <gemma@halfapenguin.com>
2021-02-11 18:10:45 +0000

    Switch comments to help context

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -40,26 +40,34 @@ clean-bytecode:
 	@echo "> Removing compiled bytecode..."
 	@find controlpanel -name '__pycache__' -d -prune -exec rm -r {} +
 
-prepare-dev-up:
+## dev-prepare-up: Run migration before doing up
+dev-prepare-up:
 	docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml run migration
 
-dev-daemon: prepare-dev-up # Startup with docker process in background (to stop afterwards use make clean)
+## dev-daemon: Startup with docker process in background (to stop afterwards use make clean)
+dev-daemon: dev-prepare-up
 	aws-vault exec restricted-data -- docker-compose -f docker-compose.yaml -f  docker-compose.dev.yaml up -d frontend
 
-dev-fg: prepare-dev-up # Startup with docker process in foreground
+## dev-fg: Startup with docker process in foreground
+dev-fg: dev-prepare-up
 	aws-vault exec restricted-data -- docker-compose -f docker-compose.yaml -f  docker-compose.dev.yaml up frontend
 
-dev-debug: clean dev-daemon # Startup clean docker process in background, and docker attach to foreground for debugging
+## dev-debug: Startup clean docker process in background, and docker attach to foreground for debugging
+dev-debug: clean dev-daemon
 	docker attach $(shell sh -c "docker-compose ps -q frontend")
 
-dev-attach: # Attach to existing running background docker process for purposes of debugging
+## dev-attach: Attach to existing running background docker process for purposes of debugging
+dev-attach:
 	docker attach $(shell sh -c "docker-compose ps -q frontend")
 
-dev-py: # Start django shell (in the dev-packages context) in new container
+## dev-py: Start django shell (in the dev-packages context) in new container
+dev-py:
 	docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml run frontend sh -c "dev-packages/bin/python3 manage.py shell"
 
-dev-run: # Start shell in new copy of container
+## dev-run: Start shell in new copy of container
+dev-run:
 	docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml run --rm frontend sh
 
-dev-exec: # Exec into shell of existing container
+## dev-exec: Exec into shell of existing container
+dev-exec:
 	docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml exec frontend sh
