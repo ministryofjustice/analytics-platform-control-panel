c983d889f4a0e4670cdc43ff744a7b64492d1324
Nicholas H.Tollervey <ntoll@ntoll.org>
2021-08-20 14:44:34 +0100

    Use message middleware to display message telling users in a pending state that they should migrate.

--- controlpanel/middleware/__init__.py
+++ controlpanel/middleware/__init__.py
@@ -1,2 +1,3 @@
 from controlpanel.middleware.never_cache import DisableClientSideCachingMiddleware
 from controlpanel.middleware.legacy_api_redirect import LegacyAPIRedirectMiddleware
+from controlpanel.middleware.migrate_alert import MigrateAlertMiddleware
