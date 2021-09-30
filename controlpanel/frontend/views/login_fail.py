0785dcc42c8af5c0dcda06bdad26f5a7b82d6e0e
Nicholas H.Tollervey <ntoll@ntoll.org>
2021-09-30 11:30:30 +0100

    Refactors copy and URLs of the old login-fail page to make it more informative and work properly with EKS.

--- controlpanel/frontend/views/login_fail.py
+++ controlpanel/frontend/views/login_fail.py
@@ -8,4 +8,5 @@ class LoginFail(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["environment"] = settings.ENV 
+        context["EKS"] = settings.EKS
         return context
