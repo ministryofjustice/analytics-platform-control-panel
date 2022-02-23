e26068f5b9f46446c9ac0c26287fa0e4101d5e39
Ross Jones <ross.jones@digital.justice.gov.uk>
2022-02-23 08:43:38 +0000

    Add new test for mixed case emails

--- tests/api/test_auth0.py
+++ tests/api/test_auth0.py
@@ -16,7 +16,7 @@ def ManagementAPI():
 
 @pytest.yield_fixture
 def fixture_users_200(AuthorizationAPI):
-    with patch.object(AuthorizationAPI, 'request') as request:
+    with patch.object(AuthorizationAPI, "request") as request:
         request.side_effect = [
             {
                 "total": 200,
@@ -36,7 +36,7 @@ def fixture_users_200(AuthorizationAPI):
 
 @pytest.yield_fixture
 def fixture_groups(AuthorizationAPI):
-    with patch.object(AuthorizationAPI, 'request') as request:
+    with patch.object(AuthorizationAPI, "request") as request:
         request.side_effect = [
             {
                 "total": 2,
@@ -87,17 +87,18 @@ def test_delete_group(AuthorizationAPI, fixture_get_group):
             {"_id": role_id, "permissions": [permission_id]},
         ]
 
-
         AuthorizationAPI.delete_group(group_name="foo")
 
         fixture_get_group.assert_called_with("foo")
 
-        request.assert_has_calls([
-            call("GET", f"groups/{group_id}/roles"),
-            call("DELETE", f"groups/{group_id}"),
-            call("DELETE", f"roles/{role_id}"),
-            call("DELETE", f"permissions/{permission_id}"),
-        ])
+        request.assert_has_calls(
+            [
+                call("GET", f"groups/{group_id}/roles"),
+                call("DELETE", f"groups/{group_id}"),
+                call("DELETE", f"roles/{role_id}"),
+                call("DELETE", f"permissions/{permission_id}"),
+            ]
+        )
 
 
 def test_create_user(ManagementAPI):
@@ -105,62 +106,119 @@ def test_create_user(ManagementAPI):
         email = "foo@test.com"
         nickname = "foo"
         request.return_value = {
-            'email': 'foo@test.com',
-            'email_verified': True,
-            'identities': [
+            "email": "foo@test.com",
+            "email_verified": True,
+            "identities": [
                 {
-                    'connection': 'email',
-                    'user_id': '61f9b45470ad3d31400c8cec',
-                    'provider': 'email',
-                    'isSocial': False
+                    "connection": "email",
+                    "user_id": "61f9b45470ad3d31400c8cec",
+                    "provider": "email",
+                    "isSocial": False,
                 }
             ],
-            'name': 'foot@test.com',
-            'nickname': 'foo',
-            'user_id': 'email|61f9b45470ad3d31400c8cec'
+            "name": "foot@test.com",
+            "nickname": "foo",
+            "user_id": "email|61f9b45470ad3d31400c8cec",
         }
 
         ManagementAPI.create_user(email=email, email_verified=True, connection="email")
 
-        request.assert_has_calls([
-            call(
-                "POST", 
-                "users",
-                json={"email": email, "email_verified": True, "connection": "email", "nickname": nickname}
-            )
-        ])
+        request.assert_has_calls(
+            [
+                call(
+                    "POST",
+                    "users",
+                    json={
+                        "email": email,
+                        "email_verified": True,
+                        "connection": "email",
+                        "nickname": nickname,
+                    },
+                )
+            ]
+        )
+
+
+@pytest.yield_fixture
+def fixture_get_users_email_search_empty(AuthorizationAPI):
+    with patch.object(
+        AuthorizationAPI.mgmt, "get_users_email_search"
+    ) as get_users_email_search:
+        get_users_email_search.return_value = []
+        yield get_users_email_search
 
 
 @pytest.yield_fixture
 def fixture_get_users_email_search(AuthorizationAPI):
-    with patch.object(AuthorizationAPI.mgmt, "get_users_email_search") as get_users_email_search:
-            get_users_email_search.return_value = []
-            yield get_users_email_search
+    with patch.object(
+        AuthorizationAPI.mgmt, "get_users_email_search"
+    ) as get_users_email_search:
+        get_users_email_search.return_value = [
+            {
+                "email": "new@test.com",
+                "email_verified": True,
+                "identities": [
+                    {
+                        "connection": "email",
+                        "user_id": "new_id",
+                        "provider": "email",
+                        "isSocial": False,
+                    }
+                ],
+                "name": "foot@test.com",
+                "nickname": "foo",
+                "user_id": "email|new_id",
+            }
+        ]
+        yield get_users_email_search
 
 
 @pytest.yield_fixture
 def fixture_create_user(AuthorizationAPI):
     with patch.object(AuthorizationAPI.mgmt, "create_user") as create_user:
         create_user.return_value = {
-            'email': 'foo@test.com',
-            'email_verified': True,
-            'identities': [
+            "email": "foo@test.com",
+            "email_verified": True,
+            "identities": [
                 {
-                    'connection': 'email',
-                    'user_id': 'new_id',
-                    'provider': 'email',
-                    'isSocial': False
+                    "connection": "email",
+                    "user_id": "new_id",
+                    "provider": "email",
+                    "isSocial": False,
                 }
             ],
-            'name': 'foot@test.com',
-            'nickname': 'foo',
-            'user_id': 'email|new_id'
+            "name": "foot@test.com",
+            "nickname": "foo",
+            "user_id": "email|new_id",
         }
         yield create_user
 
 
-@pytest.mark.indevelopment
-def test_new_user_add_to_group(AuthorizationAPI,
+
+def test_new_user_add_to_group(
+    AuthorizationAPI,
+    fixture_groups,
+    fixture_get_group,
+    fixture_get_users_email_search_empty,
+    fixture_create_user,
+):
+    with patch.object(AuthorizationAPI, "request") as auth_request:
+        group_id = "foo-id"
+        group_name = "foo"
+        email = "new@test.com"
+        nickname = "new"
+        new_id = "new_id"
+
+        response = AuthorizationAPI.add_group_members(
+            emails=[email], group_name="foo", user_options={"connection": "email"}
+        )
+
+        auth_request.assert_has_calls(
+            [call("PATCH", f"groups/{group_id}/members", json=[f"email|{new_id}"])]
+        )
+
+
+def test_existing_user_add_to_group(AuthorizationAPI,
                                fixture_groups,
                                fixture_get_group,
                                fixture_get_users_email_search,
@@ -169,7 +227,7 @@ def test_new_user_add_to_group(AuthorizationAPI,
     with patch.object(AuthorizationAPI, "request") as auth_request:
         group_id = "foo-id"
         group_name = "foo"
-        email = "new@test.com"
+        email = "New@test.com"
         nickname = "new"
         new_id = "new_id"
 
