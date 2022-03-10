d3a4ebc7283221b72cefa47482bbcfa4a0af841c
Ross Jones <ross.jones@digital.justice.gov.uk>
2022-03-10 08:29:12 +0000

    Adds admin tools for S3Buckets and Apps
    Currently there is no easy way to view/modify this data short of using
    the REPL.  This PR adds tools for the Apps and the S3Buckets so that we
    can obtain more info about them (e.g. who created them and when)

--- controlpanel/api/admin.py
+++ controlpanel/api/admin.py
@@ -1,4 +1,4 @@
-from controlpanel.api.models.user import User
+from controlpanel.api.models import App, User, S3Bucket
 from django.contrib import admin
 
 
@@ -9,12 +9,34 @@ def make_migration_pending(modeladmin, request, queryset):
 make_migration_pending.short_description = "Mark selected users as pending migration"
 
 
+class AppAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_by", "created")
+    list_filter = ("created_by",)
+    search_fields = (
+        "name",
+        "description",
+        "slug",
+    )
+
+
+class S3Admin(admin.ModelAdmin):
+    list_display = ("name", "created_by", "created", "is_data_warehouse")
+    list_filter = ("created_by", "is_data_warehouse")
+    search_fields = ("name",)
+
+
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "auth0_id", "email", "is_superuser", "migration_state")
     actions = [make_migration_pending]
     exclude = ("password",)
     list_filter = ("migration_state",)
-    search_fields = ("username", "email", "auth0_id",)
+    search_fields = (
+        "username",
+        "email",
+        "auth0_id",
+    )
 
 
+admin.site.register(App, AppAdmin)
+admin.site.register(S3Bucket, S3Admin)
 admin.site.register(User, UserAdmin)
