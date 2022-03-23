bda15eff0431c27ee85a72bbaeeb45dba2bde81d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-03-23 12:32:20 +0000

    Piggyback on kind docker network and revert HOSTNAMES
    Co-authored-by: Louise Bowler <LouiseABowler@users.noreply.github.com>
    Co-authored-by: Gemma Hentsch <gemma@halfapenguin.com>

--- controlpanel/develop/views.py
+++ controlpanel/develop/views.py
@@ -1,14 +1,40 @@
+import json
+import subprocess
+from os import environ
 from typing import List
 
+from controlpanel.api.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+def run_command(command, *args):
+        env = environ.copy()
+        output = subprocess.Popen(
+            [command, *args],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            encoding="utf8",
+            env=env,
+        )
+        out, err = output.communicate()
+        return out, err
 
 
 def installed_tools(username: str) -> List[str]:
     # TODO: Get a list of this user's installed tools and return
     # a list of string ["like", "this"]
 
+    user = User.objects.get(username=username)
+    raw_cmd = f"kubectl get tools -n user-{username} -o json"
+    raw_bits = raw_cmd.split()
+    command = raw_bits[0]
+    args = raw_bits[1:]
+    out, err = run_command(command, *args)
+    breakpoint()
     return []
 
 
@@ -18,27 +44,12 @@ def user_selected_tool(username: str, toolname: str) -> str:
     return f"Install {toolname} for {username}"
 
 
-@login_required()
+# @login_required()
+@api_view()
 def develop_index(request):
-    status = None
-    tool = None
-
-    if request.method == "POST":
-        data = request.POST
-
-        tool = data.get("tool", "")
-        if not tool:
-            status = "No tool selected"
-        else:
-            status = user_selected_tool(request.user, tool)
-
-    return render(
-        request,
-        "develop/index.html",
-        {
-            "username": request.user,
-            "status": status,
-            "tool": tool,
-            "installed_tools": installed_tools(request.user),
-        },
-    )
+    raw_cmd = f"kubectl get pods -A -o json"
+    raw_bits = raw_cmd.split()
+    command = raw_bits[0]
+    args = raw_bits[1:]
+    out, err = run_command(command, *args)
+    return Response(json.loads(out))
