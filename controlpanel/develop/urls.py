40e86206b22111a7c265e7b10927389884149f09
Gemma Hentsch <gemma@halfapenguin.com>
2022-04-22 14:55:07 +0100

    Adding back in the kubernetes is_connected view
    Co-authored-by: Louise Bowler <LouiseABowler@users.noreply.github.com>

--- controlpanel/develop/urls.py
+++ controlpanel/develop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import develop_index
+from .views import develop_index, is_kube_connected_view
 
 
 urlpatterns = [
+    path("kube_connected/", is_kube_connected_view, name="is_kube_connected"),
     path("", develop_index, name="develop_index"),
 ]
