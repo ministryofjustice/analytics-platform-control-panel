29527c9248640d27939470cb24de6eb4844d2c33
Gemma Hentsch <gemma@halfapenguin.com>
2022-04-26 11:17:15 +0100

    Use the right command for the worker

--- scripts/startup_dev_worker.sh
+++ scripts/startup_dev_worker.sh
@@ -2,4 +2,4 @@
 cluster_id=$EKS_CLUSTER
 aws --region eu-west-1 eks update-kubeconfig --name $cluster_id
 python replace_aws_iam_command.py
-python manage.py runserver runworker background_tasks
+python manage.py runworker background_tasks
