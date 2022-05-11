e607a7489d1cad21ceb1bb60b8027643bad7e0d8
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-05-11 12:55:02 +0100

    User temp file for configured fixtures

--- scripts/prepare_dev_tools.py
+++ scripts/prepare_dev_tools.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 import yaml
 
 import django
@@ -27,8 +28,7 @@ if __name__ == "__main__":
         tool["fields"]["values"]["authProxy.auth0.clientId"] = os.environ[f"{env_var_prefix}_AUTH_CLIENT_ID"]
         tool["fields"]["values"]["authProxy.auth0.clientSecret"] = os.environ[f"{env_var_prefix}_AUTH_CLIENT_SECRET"]
 
-    output_file = os.path.join("controlpanel", "api", "dev_fixtures", "tool_configured.yaml")
-    with open(output_file, "w") as configured_fixture_file:
-        yaml.dump(fixture_yaml, configured_fixture_file)
-
-    management.call_command("loaddata", output_file)
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".yaml") as configured_fixture_file:
+        configured_fixture_file.write(yaml.dump(fixture_yaml))
+        configured_fixture_file.flush()
+        management.call_command("loaddata", configured_fixture_file.name)
