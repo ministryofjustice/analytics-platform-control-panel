ccd97ebab4fd48acacd42e3cf54b430003150efc
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-09-07 16:35:13 +0100

    Revise model naming

--- controlpanel/api/models/__init__.py
+++ controlpanel/api/models/__init__.py
@@ -1,4 +1,4 @@
-from controlpanel.api.models.allowed_ip_range import AllowedIPRange
+from controlpanel.api.models.ip_allowlist import IPAllowlist
 from controlpanel.api.models.app import App
 from controlpanel.api.models.apps3bucket import AppS3Bucket
 from controlpanel.api.models.iam_managed_policy import IAMManagedPolicy
--- controlpanel/api/models/ip_allowlist.py
+++ controlpanel/api/models/ip_allowlist.py
@@ -3,11 +3,11 @@ from django.db import models
 from auditlog.models import AuditlogHistoryField
 from auditlog.registry import auditlog
 
-class AllowedIPRange(models.Model):
+class IPAllowlist(models.Model):
     name = models.CharField(max_length=100, blank=False, unique=True)
     description = models.CharField(max_length=100, blank=True)
     contact = models.CharField(max_length=100, blank=True)
-    allowed_ip_range = models.TextField(blank=False)
+    allowed_ip_ranges = models.TextField(blank=False)
     history = AuditlogHistoryField()
 
-auditlog.register(AllowedIPRange)
+auditlog.register(IPAllowlist)
