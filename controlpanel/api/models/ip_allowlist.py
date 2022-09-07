3b1081ec3d2fed2bddd873aa9b376370bb1f5d33
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-09-07 18:11:39 +0100

    Make IPAllowlist a TimeStampedModel

--- controlpanel/api/models/ip_allowlist.py
+++ controlpanel/api/models/ip_allowlist.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 from simple_history.models import HistoricalRecords
 
-class IPAllowlist(models.Model):
+class IPAllowlist(TimeStampedModel):
     name = models.CharField(max_length=100, blank=False, unique=True)
     description = models.CharField(max_length=100, blank=True)
     contact = models.CharField(max_length=100, blank=True)
