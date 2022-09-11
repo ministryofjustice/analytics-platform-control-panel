d2d627d7ea3e221ae0fa4bee75ad736a54cb69e6
ahbensiali <112475984+ahbensiali@users.noreply.github.com>
2022-09-11 19:05:35 +0100

    ANPL-1049
    added button to webapp view, navigate to secrets view
    created views for secrets. Created:
    - view
    - add/update
    - app mixin for getting app and success url redirect
    
    SecretAddViewSet
    - get request returns existing secrets set
    - get request displays all allowed key secrets
    - context to send displayable details
    
    SecretAddUpdate
    - get form from url
    - get context to populate html
    - validate form. process add/update secret

--- controlpanel/frontend/views/secrets.py
+++ controlpanel/frontend/views/secrets.py
@@ -1,43 +1,80 @@
-from django import forms
-from controlpanel.frontend.forms import SecretsForm
+import secrets
+from typing import Optional
+from controlpanel.frontend.forms import SecretsForm, DisableAuthForm
 from controlpanel.oidc import OIDCLoginRequiredMixin
 from rules.contrib.views import PermissionRequiredMixin
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
-from dataclasses import dataclass
+from controlpanel.api.models import App
+from controlpanel.api import aws
+
 from django.views.generic import TemplateView
-
-@dataclass
-class InputValue:
-    key: str
-    value: str
-    form: forms.Form = SecretsForm
+from django.views.generic.edit import FormView
 
 
-class SecretAddViewSet(OIDCLoginRequiredMixin, PermissionRequiredMixin, TemplateView):
-    permission_required = 'api.create_app'
-    template_name = 'secret-add-variable.html'
-    allowed_secrets = [
-        InputValue('disable_authentication', 'Disable Authentication')
-    ]
-    success_url = 'manage-app'
+class AppSecretMixin:
+    def _get_app(self, pk) -> Optional[App]:
+        # get app from pk
+        return get_object_or_404(App, pk=pk)
     
     def get_success_url(self) -> str:
         return reverse_lazy(self.success_url, kwargs={'pk': self.kwargs.get('pk')})
 
-    def get(self, request, pk=None, *args, **kwargs):
-        return super(SecretAddViewSet, self).get(request, pk=pk, *args, **kwargs)
 
-    def post(self, request, pk=None, *args, **kwargs):
-        return super(SecretAddViewSet, self).post(request, pk=pk, *args, **kwargs)
+class SecretAddViewSet(OIDCLoginRequiredMixin, PermissionRequiredMixin, AppSecretMixin, TemplateView):
+    permission_required = 'api.create_app'
+    template_name = 'secret-view-variable.html'
+    allowed_secrets = {
+        'generic': SecretsForm,
+        'disable_authentication': DisableAuthForm
+    }
+    success_url = 'manage-app'
+
+    def get(self, request, pk=None, *args, **kwargs):
+        # get all set secrets
+        app = self._get_app(pk)
+        set_secrets = aws.AWSSecretManager().get_secret_if_found(app.app_aws_secret_name)
+        set_secrets = [key for key, _ in self.allowed_secrets.items() if key in set_secrets]
+
+        return super(SecretAddViewSet, self).get(request, *args, pk=pk, set_secrets=set_secrets, **kwargs)
 
     def get_context_data(self, *args, pk=None, **kwargs):
         context = super(SecretAddViewSet, self).get_context_data(**kwargs)
-        context['allowed_key_select'] = self.allowed_secrets
-        context['pk'] = self.kwargs.get('pk')
-
-        context['forms'] = {}
-        for form in self.allowed_secrets:
-            context['forms'][form.key] = form.form(initial={'secret_key': form.key})
-        
+        context['forms'] = self.allowed_secrets
+        context['pk'] = pk        
         return context
+
+
+class SecretAddUpdate(OIDCLoginRequiredMixin, PermissionRequiredMixin, AppSecretMixin, FormView):
+    permission_required = 'api.create_app'
+    template_name = 'secret-add-variable.html'
+    success_url = 'view-secret'
+    allowed_keys = {
+        'generic': SecretsForm,
+        'disable_authentication': DisableAuthForm,
+    }
+
+    def get_form(self, form_class):
+        # secret_key received from the url
+        form_key = self.kwargs.get('secret_key', 'generic')
+        form_class = self.allowed_keys.get(form_key)
+        return form_class
+
+    def get_context_data(self, **kwargs):
+        secret_key = self.kwargs.get('secret_key')
+        form = self.allowed_keys.get(secret_key)()
+        return super().get_context_data(secret_key=secret_key, form=form, **kwargs)
+
+    def form_valid(self, form):
+        secret_key = self.kwargs.get('secret_key')
+        secret_value = form.cleaned_data.get('secret_value')
+        app = self._get_app(self.kwargs.get('pk'))
+
+        if not self.allowed_keys.get(secret_key):
+            raise Http404("Invalid Webapp")
+
+        # update secret key
+        aws.AWSSecretManager().create_or_update(app.app_aws_secret_name, {'secret_key': secret_value})
+        return super(SecretAddUpdate, self).form_valid(form)
