96d7a577d94375c12a0ebf365a23dff41c52beba
ahbensiali <112475984+ahbensiali@users.noreply.github.com>
2022-09-27 09:51:16 +0100

    ANPL-1049 added doc link to how-to permissions. removed mandatory field

--- controlpanel/frontend/forms.py
+++ controlpanel/frontend/forms.py
@@ -65,7 +65,7 @@ class CreateAppForm(forms.Form):
         ],
     )
 
-    disable_authentication = forms.BooleanField(required=True)
+    disable_authentication = forms.BooleanField(required=False)
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
