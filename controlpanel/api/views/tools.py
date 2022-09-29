815029f907045ef393d930e5c37669ccc4314f34
ahbensiali <112475984+ahbensiali@users.noreply.github.com>
2022-09-29 08:27:45 +0100

    ANPL-987 reformatted & flake8 pass

--- controlpanel/api/views/tools.py
+++ controlpanel/api/views/tools.py
@@ -1,20 +1,14 @@
-from rest_framework import mixins, status, viewsets
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from controlpanel.api import permissions
-from controlpanel.api.github import GithubAPI
-
-from controlpanel.api import permissions
-from controlpanel.api.serializers import (
-    ToolSerializer,
-    GithubSerializer,
-)
-from controlpanel.api.models import (
-    Tool,
-    ToolDeployment,
-)
 from django.conf import settings
 from django.core.cache import cache
+from rest_framework import mixins, viewsets
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from controlpanel.api import permissions
+from controlpanel.api.github import GithubAPI
+from controlpanel.api.models import Tool
+from controlpanel.api.serializers import GithubSerializer, ToolSerializer
+
 
 class ToolViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     filter_backends = []
@@ -27,20 +21,23 @@ class ToolViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 class RepoApi(GenericAPIView):
     serializer_class = GithubSerializer
     permission_classes = (permissions.AppPermissions,)
-    action = 'retrieve'
+    action = "retrieve"
 
     def get_queryset(self):
         return []
 
     def query(self, org: str, page: int):
-        cache_key = f'{org}:repos:{page}'
+        cache_key = f"{org}:repos:{page}"
         result = cache.get(cache_key)
         if result is not None and len(result):
             return result
 
-        repos = GithubAPI(self.request.user.github_api_token).get_repos(org, page)
+        token = self.request.user.github_api_token
+        repos = GithubAPI(token).get_repos(org, page)
         result = [
-            dict(html_url=repo.get('html_url'), full_name=repo.get('full_name')) for repo in repos if not repo.get('archived')
+            {"html_url": r.get("html_url"), "full_name": r.get("full_name")}
+            for r in repos
+            if not r.get("archived")
         ]
 
         timeout = page * 20
@@ -52,9 +49,9 @@ class RepoApi(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         data = request.GET.dict()
-        page = data.get('page', 1)
-        org = data.get('org', settings.GITHUB_ORGS[0])
-        
+        page = data.get("page", 1)
+        org = data.get("org", settings.GITHUB_ORGS[0])
+
         repos = self.query(org, int(page))
 
         serializers = self.get_serializer(data=repos, many=True)
