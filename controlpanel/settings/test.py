f8446afb3b52f6926f63a6d2038f34237a0b998c
ahbensiali <112475984+ahbensiali@users.noreply.github.com>
2022-10-28 09:12:29 +0100

    ANPL-862 conflict fix

--- controlpanel/settings/test.py
+++ controlpanel/settings/test.py
@@ -1,28 +1,29 @@
+# First-party/Local
 from controlpanel.settings.common import *
 
-ENV = 'test'
+ENV = "test"
 
 AWS_COMPUTE_ACCOUNT_ID = "test_compute_account_id"
 AWS_DATA_ACCOUNT_ID = "123456789012"  # XXX DO NOT CHANGE - it will break moto tests
 K8S_WORKER_ROLE_NAME = "nodes.example.com"
 SAML_PROVIDER = "test-saml"
 
-LOGGING["loggers"]["django_structlog"]["level"] = "WARNING"
-LOGGING["loggers"]["controlpanel"]["level"] = "WARNING"
+LOGGING["loggers"]["django_structlog"]["level"] = "WARNING"  # noqa: F405
+LOGGING["loggers"]["controlpanel"]["level"] = "WARNING"  # noqa: F405
 
 AUTHENTICATION_BACKENDS = [
-    'rules.permissions.ObjectPermissionBackend',
-    'django.contrib.auth.backends.ModelBackend',
+    "rules.permissions.ObjectPermissionBackend",
+    "django.contrib.auth.backends.ModelBackend",
 ]
-MIDDLEWARE.remove('mozilla_django_oidc.middleware.SessionRefresh')
-REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'].remove(
-    'mozilla_django_oidc.contrib.drf.OIDCAuthentication',
+MIDDLEWARE.remove("mozilla_django_oidc.middleware.SessionRefresh")  # noqa: F405
+REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].remove(  # noqa: F405
+    "mozilla_django_oidc.contrib.drf.OIDCAuthentication",
 )
 OIDC_OP_JWKS_ENDPOINT = "https://example.com/.well-known/jwks.json"
 OIDC_ALLOW_UNSECURED_JWT = True
 OIDC_DOMAIN = "oidc.idp.example.com"
 
-TOOLS_DOMAIN = 'example.com'
+TOOLS_DOMAIN = "example.com"
 
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
