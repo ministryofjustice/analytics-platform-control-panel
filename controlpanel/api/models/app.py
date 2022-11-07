df32f38fbf44b6beef05767e07bdac78227adf51
ahbensiali <112475984+ahbensiali@users.noreply.github.com>
2022-11-07 09:34:37 +0000

    ANPL-1201 js for frontend. API endpoint for Auth0 users. addition of helper func on App model

--- controlpanel/api/models/app.py
+++ controlpanel/api/models/app.py
@@ -42,6 +42,12 @@ class App(TimeStampedModel):
     def get_logs(self, num_hours=None):
         return elasticsearch.app_logs(self, num_hours=num_hours)
 
+    def customer_paginated(self, page, per_page=25):
+        return (
+            auth0.ExtendedAuth0().groups.get_group_members_paginated(group_name=self.slug, page=page, per_page=per_page) or []
+        )
+        
+
     @property
     def customers(self):
         return (
