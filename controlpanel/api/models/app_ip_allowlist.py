362c70eb4f4649d5e00cce98d94d7be7385a1718
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-03-09 17:03:51 +0000

    ANPL-1414 reformatted changed file under api/models

--- controlpanel/api/models/app_ip_allowlist.py
+++ controlpanel/api/models/app_ip_allowlist.py
@@ -6,29 +6,26 @@ from controlpanel.api import cluster
 
 
 class AppIPAllowListManager(models.Manager):
-
     def update_records(self, app, env_name, ip_allowlists):
         for related_item in app.appipallowlists.filter(deployment_env=env_name):
             related_item.delete()
         for ip_range_item in ip_allowlists:
             AppIPAllowList.objects.create(
-                app_id=app.id,
-                ip_allowlist_id=ip_range_item.id,
-                deployment_env=env_name
+                app_id=app.id, ip_allowlist_id=ip_range_item.id, deployment_env=env_name
             )
 
     def update_ip_allowlist(self, *args, **kwargs):
         app = kwargs.get("app")
         github_api_token = kwargs.get("github_api_token")
-        env_name = kwargs.get('env_name')
-        ip_allowlists = kwargs.get('ip_allowlists')
+        env_name = kwargs.get("env_name")
+        ip_allowlists = kwargs.get("ip_allowlists")
         self.update_records(app, env_name, ip_allowlists)
 
         cluster.App(app).create_or_update_secret(
             github_token=github_api_token,
             env_name=env_name,
             secret_key=cluster.App.IP_RANGES,
-            secret_value=app.env_allowed_ip_ranges(env_name=env_name)
+            secret_value=app.env_allowed_ip_ranges(env_name=env_name),
         )
 
 
