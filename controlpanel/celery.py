f67567438391dcadd16b8dd306cea2a9d8255de5
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-08-02 14:54:34 +0100

    ANPL-0000 Added health check for celery check

--- controlpanel/celery.py
+++ controlpanel/celery.py
@@ -1,13 +1,14 @@
 import os
-import dotenv
 from celery import Celery
-from time import time
-import json
-
+import dotenv
 from kombu import Queue
+from pathlib import Path
+import structlog
 
 # First-party/Local
 from controlpanel.utils import load_app_conf_from_file
+from django.conf import settings
+
 dotenv.load_dotenv()
 
 
@@ -25,10 +26,18 @@ app.config_from_object('django.conf:settings')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+log = structlog.getLogger(__name__)
+
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+
+@app.task(bind=True, ignore_result=True)
+def worker_health_check(self):
+    Path(settings.WORKER_HEALTH_FILENAME).touch()
+    log.debug("Worker health ping task executed")
 
 
 # ensures worker picks and runs tasks from all queues rather than just default queue
