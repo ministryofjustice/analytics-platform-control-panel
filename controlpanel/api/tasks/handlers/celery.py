7878022e3fdc35e7557cf0f9a3fe1c4bd6d2cbb1
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-08-04 16:54:27 +0100

    Move permission checks to base class

--- controlpanel/api/tasks/handlers/celery.py
+++ controlpanel/api/tasks/handlers/celery.py
@@ -5,10 +5,12 @@ from controlpanel.api import cluster
 from controlpanel.api.models import App, User, S3Bucket, UserS3Bucket, AppS3Bucket, Task
 
 
-class BaseTaskHandler(CeleryTask):
-
+class BaseModelTaskHandler(CeleryTask):
+    name = None
     model = None
-    # can be applied to project settings
+    permission_required = None
+
+    # can be applied to project settings also
     # these settings mean that messages are only removed from the queue (acknowledged)
     # when returned. if an error occurs, they remain in the queue, and will be resent
     # to the worker when the "visibility_timeout" has expired. "visibility_timeout" is
@@ -25,12 +27,30 @@ class BaseTaskHandler(CeleryTask):
             raise exc
 
     def get_user(self, pk):
+        """
+        Try to find the user, then check they have the correct permission required to
+        run the task action.
+        """
         try:
             return User.objects.get(pk=pk)
         except User.DoesNotExist as exc:
             # if the user is found, this should be a hard fail? So suggest log the error
             # and then mark as complete to stop task being rerun?
-            return self.complete()
+            return None
+
+    def has_permission(self, user, obj=None):
+        """
+        Check that the user has permission to run the task on the given object.
+        Override on the subclass for further permission checks.
+        """
+        if not self.permission_required:
+            raise NotImplementedError("Must define a permission to check")
+
+        if not user.has_perm(self.permission_required, obj=obj):
+            # log that the user did not have permission?
+            return False
+
+        return True
 
     def complete(self):
         task = Task.objects.filter(task_id=self.request.id).first()
@@ -38,24 +58,42 @@ class BaseTaskHandler(CeleryTask):
             task.completed = True
             task.save()
 
+    def run(self, obj_pk, user_pk, *args, **kwargs):
+        """
+        Default message that a celery Task object requires to be defined, and will be
+        called by the worker when a message is received by the queue. This runs some
+        lookups and validates the user, and if these pass calls `run_task` which must
+        be defined on any subclass of BaseTaskHandler.
+        """
+        obj = self.get_object(obj_pk)
+        user = self.get_user(user_pk)
+        if not user:
+            return self.complete()
 
-class CreateAppAuthSettings(BaseTaskHandler):
+        if not self.has_permission(user, obj):
+            return self.complete()
+
+        self.run_task(obj, user, *args, **kwargs)
+
+    def run_task(self, *args, **kwargs):
+        """
+        Should contain the logic to run the task, and will be called after the run
+        method has been successfully called.
+        """
+        raise NotImplementedError("Task logic not implemented")
+
+
+class CreateAppAuthSettings(BaseModelTaskHandler):
     model = App
     name = "create_app_auth_settings"
     permission_required = "api.create_app"
 
-    def run(self, app_pk, user_pk, envs, disable_authentication, connections):
-        user = self.get_user(pk=user_pk)
-        if not user.has_perm(self.permission_required):
-            # suggest send error to sentry with capture_messgae then mark complete so it
-            # doesnt run again?
-            return self.complete()
-
-        app = self.get_object(pk=app_pk)
+    def has_permission(self, user, obj=None):
         if not user.github_api_token:
-            # should a task that cannot be completed be marked as complete in DB?
-            return self.complete()
+            return False
+        return super().has_permission(user, obj)
 
+    def run_task(self, app, user, envs, disable_authentication, connections):
         for env in envs:
             cluster.App(app, user.github_api_token).create_auth_settings(
                 env_name=env,
@@ -65,64 +103,32 @@ class CreateAppAuthSettings(BaseTaskHandler):
         self.complete()
 
 
-class CreateS3Bucket(BaseTaskHandler):
-
+class CreateS3Bucket(BaseModelTaskHandler):
     model = S3Bucket
     name = "create_s3bucket"
     permission_required = "api.create_s3bucket"
 
-    def run(self, bucket_pk, user_pk, bucket_owner="APP"):
-        bucket = self.get_object(pk=bucket_pk)
-        # this will already have run when the obj was created via modelform validation.
-        # Is it unnecessary to call again?
-        if cluster.S3Bucket(bucket).exists(bucket.name, bucket_owner=bucket_owner):
-            # should a task that cannot be completed be marked as complete in DB?
-            return self.complete()
-
-        user = self.get_user(pk=user_pk)
-        if not user.has_perm(self.permission_required):
-            # suggest send error to sentry with capture_messgae then mark complete so it
-            # doesnt run again?
-            return self.complete()
-
+    def run_task(self, bucket, user, bucket_owner="APP"):
         bucket.cluster.create(owner=bucket_owner)
         self.complete()
 
 
-class CreateAppAWSRole(BaseTaskHandler):
+class CreateAppAWSRole(BaseModelTaskHandler):
     model = App
     name = "create_app_aws_role"
     permission_required = "api.create_app"
 
-    def run(self, app_pk, user_pk):
-        app = self.get_object(pk=app_pk)
-        user = self.get_user(pk=user_pk)
-        if not user or not user.has_perm(self.permission_required, app):
-            # suggest send error to sentry with capture_messgae then mark complete so it
-            # doesnt run again?
-            return self.complete()
-
+    def run_task(self, app, user):
         cluster.App(app).create_iam_role()
         self.complete()
 
 
-class GrantAppS3BucketAccess(BaseTaskHandler):
-
+class GrantAppS3BucketAccess(BaseModelTaskHandler):
     model = AppS3Bucket
     name = 'grant_app_s3bucket_access'
     permission_required = 'api.create_apps3bucket'
 
-    def run(self, app_s3_bucket_pk, user_pk):
-        user = self.get_user(pk=user_pk)
-        if not user and not user.has_perm(self.permission_required):
-            # suggest send error to sentry with capture_messgae then mark complete so it
-            # doesnt run again?
-            return self.complete()
-
-        app_bucket = self.get_object(pk=app_s3_bucket_pk)
-
-        # TODO verify user?
-
+    def run_task(self, app_bucket, user):
         cluster.App(app_bucket.app).grant_bucket_access(
             app_bucket.s3bucket.arn,
             app_bucket.access_level,
@@ -131,21 +137,12 @@ class GrantAppS3BucketAccess(BaseTaskHandler):
         self.complete()
 
 
-class GrantUserS3BucketAccess(BaseTaskHandler):
-
+class GrantUserS3BucketAccess(BaseModelTaskHandler):
     model = UserS3Bucket
     name = "grant_user_s3bucket_access"
     permission_required = "api.create_users3bucket"
 
-    def run(self, bucket_pk, user_pk):
-        user = self.get_user(pk=user_pk)
-        if not user.has_perm(self.permission_required):
-            # suggest send error to sentry with capture_messgae then mark complete so it
-            # doesnt run again?
-            return self.complete()
-
-        user_bucket = self.get_object(pk=bucket_pk)
-
+    def run_task(self, user_bucket, user):
         cluster.User(user_bucket.user).grant_bucket_access(
             user_bucket.s3bucket.arn,
             user_bucket.access_level,
--- tests/api/tasks/test_create_app_aws_role.py
+++ tests/api/tasks/test_create_app_aws_role.py
@@ -8,24 +8,24 @@ from controlpanel.api.tasks.handlers.celery import create_app_aws_role
 
 
 @pytest.mark.django_db
-@patch("controlpanel.api.tasks.handlers.celery.BaseTaskHandler.complete")
+@patch("controlpanel.api.tasks.handlers.celery.BaseModelTaskHandler.complete")
 @patch("controlpanel.api.tasks.handlers.celery.cluster")
 def test_cluster_not_called_without_valid_app(cluster, complete, users):
     with pytest.raises(App.DoesNotExist):
-        create_app_aws_role(app_pk=1, user_pk=users["superuser"].pk)
+        create_app_aws_role(1, users["superuser"].pk)
         cluster.App.assert_not_called()
         # should not be complete as we want to try it again
         complete.assert_not_called()
 
 
 @pytest.mark.django_db
-@patch("controlpanel.api.tasks.handlers.celery.BaseTaskHandler.complete")
+@patch("controlpanel.api.tasks.handlers.celery.BaseModelTaskHandler.complete")
 @patch("controlpanel.api.tasks.handlers.celery.cluster")
 def test_cluster_not_called_without_valid_user(cluster, complete, users):
     app = mommy.make("api.App")
 
     user = users["normal_user"]
-    create_app_aws_role(app_pk=app.pk, user_pk=user.pk)
+    create_app_aws_role(app.pk, user.pk)
 
     # role should not be created
     cluster.App.assert_not_called()
@@ -34,12 +34,12 @@ def test_cluster_not_called_without_valid_user(cluster, complete, users):
 
 
 @pytest.mark.django_db
-@patch("controlpanel.api.tasks.handlers.celery.BaseTaskHandler.complete")
+@patch("controlpanel.api.tasks.handlers.celery.BaseModelTaskHandler.complete")
 @patch("controlpanel.api.tasks.handlers.celery.cluster")
 def test_valid_app_and_user(cluster, complete, users):
     app = mommy.make("api.App")
 
-    create_app_aws_role(app_pk=app.pk, user_pk=users["superuser"].pk)
+    create_app_aws_role(app.pk, users["superuser"].pk)
 
     cluster.App.assert_called_once_with(app)
     cluster.App.return_value.create_iam_role.assert_called_once()
