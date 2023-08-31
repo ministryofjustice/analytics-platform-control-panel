94f3871874b982d8b38d7cfbac3a2a7b201180cf
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-08-31 15:18:08 +0100

    ANPL-1704 remove debug prints

--- controlpanel/api/tasks/handlers/s3.py
+++ controlpanel/api/tasks/handlers/s3.py
@@ -36,17 +36,15 @@ class GrantUserS3BucketAccess(BaseModelTaskHandler):
 
     def run_task(self, user_bucket, user):
         if user_bucket.s3bucket.is_folder:
-            print(f"GRANTING {user_bucket.access_level} ACCESS TO A FOLDER FOR {user}")
             cluster.User(user_bucket.user).grant_folder_access(
                 root_folder_path=user_bucket.s3bucket.name,
                 access_level=user_bucket.access_level,
                 paths=user_bucket.paths,
             )
         else:
-            print(f"GRANTING {user_bucket.access_level} ACCESS TO A BUCKET FOR {user}")
             cluster.User(user_bucket.user).grant_bucket_access(
-                user_bucket.s3bucket.arn,
-                user_bucket.access_level,
-                user_bucket.resources,
+                bucket_arn=user_bucket.s3bucket.arn,
+                access_level=user_bucket.access_level,
+                path_arns=user_bucket.resources,
             )
         self.complete()
