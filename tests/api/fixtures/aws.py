7630d19384295688a501c6400ecae843184202ae
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-03-11 09:53:46 +0000

    added tests for the bedrock view

--- tests/api/fixtures/aws.py
+++ tests/api/fixtures/aws.py
@@ -129,6 +129,29 @@ def airflow_prod_policy(iam):
 
 
 @pytest.fixture(autouse=True)
+def bedrock_policy(iam):
+    result = iam.meta.client.create_policy(
+        PolicyName="bedrock-policy-name",
+        PolicyDocument=json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "BedrockEnable",
+                        "Effect": "Allow",
+                        "Action": ["iam:AllowBedrock"],
+                        "Resource": [
+                            "arn:aws:iam::{settings.AWS_DATA_ACCOUNT_ID}:role/{settings.ENV}_user_*"  # noqa: E501
+                        ],
+                    },
+                ],
+            }
+        ),
+    )
+    return result["Policy"]
+
+
+@pytest.fixture(autouse=True)
 def test_policy(iam):
     result = iam.meta.client.create_policy(
         PolicyName="a-test-policy",
