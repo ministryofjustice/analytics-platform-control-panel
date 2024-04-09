c0f56eeecce798c041dcfdc7b820a305964d816b
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-04-09 13:07:51 +0100

    added unit tests for success url property

---
+++ tests/test_oidc.py
@@ -0,0 +1,21 @@
+import pytest
+from controlpanel.oidc import StateMismatchHandler
+from unittest.mock import Mock
+
+
+@pytest.mark.parametrize(
+    "email, success_url",
+    [
+        ("", "/"),
+        ("example@justice.gov.uk", "/tools/"),
+    ],
+)
+def test_success_url(users, email, success_url):
+    request = Mock()
+    request.session.get.return_value = "/tools/"
+    user = users["normal_user"]
+    user.justice_email = email
+    view = StateMismatchHandler()
+    view.request = request
+    view.user = user
+    assert view.success_url == success_url
