14e09ba477d6f5b6c04b09911f3bd6de7c02ee81
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-04-09 10:15:05 +0100

    Removed commented out tests. Updated running.md to specify python 3.12

--- tests/api/models/test_ip_allowlist.py
+++ tests/api/models/test_ip_allowlist.py
@@ -19,19 +19,6 @@ def update_aws_secrets_manager():
         yield update_aws_secrets_manager
 
 
-# def test_ip_allowlist_save_updates_aws_secrets_manager(update_aws_secrets_manager):
-#     ip_allowlist = baker.make("api.IPAllowlist", allowed_ip_ranges="123")
-#     app = baker.make("api.App", ip_allowlists=[ip_allowlist])  # noqa:F841
-#
-#     ip_allowlist.save()
-#
-#     update_aws_secrets_manager.assert_has_calls(
-#         [
-#             call({"allowed_ip_ranges": "123"}),
-#         ]
-#     )
-#
-
 def test_ip_allowlist_without_app_save_does_not_update_aws_secrets_manager(
     update_aws_secrets_manager,
 ):
@@ -41,20 +28,6 @@ def test_ip_allowlist_without_app_save_does_not_update_aws_secrets_manager(
 
     update_aws_secrets_manager.assert_not_called()
 
-
-# def test_ip_allowlist_delete_updates_aws_secrets_manager(update_aws_secrets_manager):
-#     ip_allowlists = [
-#         baker.make("api.IPAllowlist", allowed_ip_ranges="xyz"),
-#         baker.make("api.IPAllowlist", allowed_ip_ranges="123"),
-#     ]
-#     apps = baker.make("api.App", ip_allowlists=ip_allowlists, _quantity=2)  # noqa:F841
-#
-#     ip_allowlists[0].delete()
-#
-#     update_aws_secrets_manager.assert_has_calls(
-#         [call({"allowed_ip_ranges": "123"}), call({"allowed_ip_ranges": "123"})]
-#     )
-#
 
 def test_ip_allowlist_without_app_delete_does_not_update_aws_secrets_manager(
     update_aws_secrets_manager,
