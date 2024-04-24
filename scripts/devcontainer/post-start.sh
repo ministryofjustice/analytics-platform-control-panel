e7ab0a03a857dc777ee51618f94a4c68b3702fd7
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-04-24 11:54:08 +0100

    Removed entrypoint script. Removed prod cluster add

--- scripts/devcontainer/post-start.sh
+++ scripts/devcontainer/post-start.sh
@@ -6,5 +6,4 @@ pre-commit install
 aws-sso config-profiles
 
 aws-sso exec --profile analytical-platform-development:AdministratorAccess -- aws eks --region eu-west-1 update-kubeconfig --name development-aWrhyc0m --alias dev-eks-cluster
-aws-sso exec --profile analytical-platform-production:AdministratorAccess -- aws eks --region eu-west-1 update-kubeconfig --name production-dBSvju9Y --alias prod-eks-cluster
 kubectl config use-context dev-eks-cluster
