d932c97da8bfa4c01a94572bb829fb793a8658f3
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-10-04 11:34:13 +0000

    Clean up

--- controlpanel/frontend/forms.py
+++ controlpanel/frontend/forms.py
@@ -580,22 +580,6 @@ class IPAllowlistForm(forms.ModelForm):
         ]
 
 
-# class CreateParameterForm(forms.Form):
-#     app_id = forms.CharField(widget=forms.HiddenInput)
-#     key = forms.CharField(
-#         validators=[
-#             RegexValidator(
-#                 r"[a-zA-Z0-9_]",
-#                 message=("Must contain only alphanumeric characters and underscores"),
-#             ),
-#         ],
-#     )
-#     value = forms.CharField(
-#         max_length=65536,
-#         widget=forms.PasswordInput(attrs={"class": "govuk-input cpanel-input--1-3"}),
-#     )
-
-
 class CreateParameterForm(forms.Form):
     key = forms.CharField(
         max_length=50,
