c120466d18937d740b468ea4c1b10d4c39294807
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-10-04 10:36:50 +0100

    Refactored function and fixed flake8 issues

--- controlpanel/frontend/mixins.py
+++ controlpanel/frontend/mixins.py
@@ -7,7 +7,11 @@ from controlpanel.oidc import OIDCLoginRequiredMixin
 
 
 class PolicyAccessMixin(OIDCLoginRequiredMixin, PermissionRequiredMixin):
-    """Updates policy access for a model instance. Assumes updating a boolean field and updating a policy based on it."""
+    """
+    Updates policy access for a model instance.
+    Assumes updating a boolean field and updating a policy based on it.
+    Must set success_message and method_name attributes on view.
+    """
 
     http_method_names = ["post"]
     permission_required = "api.add_superuser"
@@ -15,16 +19,11 @@ class PolicyAccessMixin(OIDCLoginRequiredMixin, PermissionRequiredMixin):
     method_name = ""
 
     def form_valid(self, form):
+        if not hasattr(self.object, self.method_name):
+            raise AttributeError(f"Method {self.method_name} not found on {self.object}")
 
-        try:
-            if not hasattr(self.object, self.method_name):
-                raise AttributeError(f"Method {self.method_name} not found on {self.object}")
-
-            getattr(self.object, self.method_name)()
-            return super().form_valid(form)
-        except Exception as e:
-            messages.error(self.request, "An error occurred while updating the policy access level")
-            return super().form_invalid(form)
+        getattr(self.object, self.method_name)()
+        return super().form_valid(form)
 
     def get_success_url(self):
         messages.success(self.request, self.success_message)
