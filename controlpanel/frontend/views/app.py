04177e0e8cc9999e17368c3d7af22a95244d6684
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-11-21 11:18:43 +0000

    Hidden additional form through feature flag

--- controlpanel/frontend/views/app.py
+++ controlpanel/frontend/views/app.py
@@ -256,6 +256,13 @@ class UpdateAppAuth0Connections(OIDCLoginRequiredMixin, PermissionRequiredMixin,
             return FormMixin.form_invalid(self, form)
         return FormMixin.form_valid(self, form)
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["show_cloud_platform_assume_role"] = (
+            settings.features.cloud_platform_assume_role.enabled
+        )
+        return context
+
 
 class UpdateAppIPAllowlists(OIDCLoginRequiredMixin, PermissionRequiredMixin, UpdateView):
 
