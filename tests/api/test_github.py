5ce8633a2233dfa066a0bca41753524f160518d6
jamesstottmoj <james.stott@digital.justice.gov.uk>
2025-02-07 09:50:06 +0000

    Ran black

--- tests/api/test_github.py
+++ tests/api/test_github.py
@@ -4,6 +4,7 @@ import pytest
 
 from controlpanel.api.github import GithubAPI, RepositoryNotFound
 
+
 @pytest.fixture()
 def requests():
     """
@@ -39,7 +40,7 @@ def request_get_not_found(requests):
 
 
 def test_get_repository_success(request_get_success):
-    
+
     test_api_token = "abc123"
     response = GithubAPI(test_api_token).get_repository("test-repo-name")
 
@@ -47,28 +48,29 @@ def test_get_repository_success(request_get_success):
 
 
 def test_get_repository_not_found(request_get_not_found):
-    
+
     with pytest.raises(
-            RepositoryNotFound, 
-            match="Repository 'test-repo-name' not found, it may be private"
-        ):
+        RepositoryNotFound, match="Repository 'test-repo-name' not found, it may be private"
+    ):
         test_api_token = "abc123"
         GithubAPI(test_api_token).get_repository("test-repo-name")
 
 
 def test_get_repository_contents_success(request_get_success):
-    
+
     test_api_token = "abc123"
-    response = GithubAPI(test_api_token).get_repository_contents("test-repo-name", "some/resource/path")
+    response = GithubAPI(test_api_token).get_repository_contents(
+        "test-repo-name", "some/resource/path"
+    )
 
     assert response["repo"] == "test-repo-name"
 
 
 def test_get_repository_contents_not_found(request_get_not_found):
-    
+
     with pytest.raises(
-            RepositoryNotFound, 
-            match="Repository path 'some/resource/path' in test-repo-name not found. It may not exist"
-        ):
+        RepositoryNotFound,
+        match="Repository path 'some/resource/path' in test-repo-name not found. It may not exist",
+    ):
         test_api_token = "abc123"
         GithubAPI(test_api_token).get_repository_contents("test-repo-name", "some/resource/path")
