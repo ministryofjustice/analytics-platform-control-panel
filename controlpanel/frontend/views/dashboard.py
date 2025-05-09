76409b9e19ee6dcc5fd66c80206f5d0007dece14
jamesstottmoj <james.stott@digital.justice.gov.uk>
2025-05-09 13:06:56 +0100

    Fixed quotes

--- controlpanel/frontend/views/dashboard.py
+++ controlpanel/frontend/views/dashboard.py
@@ -216,7 +216,7 @@ class AddDashboardCustomers(
             messages.error(
                 self.request,
                 (
-                    f"Failed to notify {", ".join(not_notified)}. "
+                    f"Failed to notify {', '.join(not_notified)}. "
                     "You may wish to email them your dahsboard link."
                 ),
             )
