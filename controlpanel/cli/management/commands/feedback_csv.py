7a728990d91c9de0b5566dc1f0f55b892fa6a161
jamesstottmoj <james.stott@digital.justice.gov.uk>
2025-11-24 17:04:40 +0000

    Removed timezone as that appears to break tests in CI

--- controlpanel/cli/management/commands/feedback_csv.py
+++ controlpanel/cli/management/commands/feedback_csv.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from io import StringIO
 
 # Third-party
-import pytz
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -28,7 +27,7 @@ class Command(BaseCommand):
         parser.add_argument("--all", "-a", action="store_true", help="Get all feedback received")
 
     def handle(self, *args, **options):
-        today = datetime.today().replace(tzinfo=pytz.utc)
+        today = datetime.today()
 
         if options["all"]:
             feedback_items = Feedback.objects.all()
